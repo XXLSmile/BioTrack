ba11423769da3f8c6d7f1b59ec64bdb817283e5f
lf0rlance <163134925+lf0rlance@users.noreply.github.com>
2025-10-15 12:35:36 -0700

    added catalog list screen, dialog, ui improvements, and ui for viewing catalog entries

--- frontend/app/src/main/java/com/cpen321/usermanagement/MainActivity.kt
+++ frontend/app/src/main/java/com/cpen321/usermanagement/MainActivity.kt
@@ -7,9 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.cpen321.usermanagement.ui.navigation.AppNavigation
+import com.cpen321.usermanagement.ui.screens.SplashScreen
 import com.cpen321.usermanagement.ui.theme.ProvideFontSizes
 import com.cpen321.usermanagement.ui.theme.ProvideSpacing
 import com.cpen321.usermanagement.ui.theme.UserManagementTheme
@@ -36,7 +37,15 @@ fun UserManagementApp() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                AppNavigation()
+                var showSplash by remember { mutableStateOf(true) }
+
+                if (showSplash) {
+                    //Animated splash before main nav appears
+                    SplashScreen(onTimeout = { showSplash = false })
+                } else {
+                    //Once splash fades out, show your app normally
+                    AppNavigation()
+                }
             }
         }
     }
--- frontend/app/src/main/java/com/cpen321/usermanagement/data/model/Catalog.kt
+++ frontend/app/src/main/java/com/cpen321/usermanagement/data/model/Catalog.kt
@@ -1,2 +1,10 @@
 package com.cpen321.usermanagement.data.model
 
+data class Catalog(
+    val id: String,
+    var name: String,
+    val createdAt: Long = System.currentTimeMillis(),
+    val entries: MutableList<CatalogEntry> = mutableListOf()
+)
+
+
