ad87ed782ababc9e4c6b767833c5a6a14f9d45ba
lf0rlance <163134925+lf0rlance@users.noreply.github.com>
2025-10-21 14:53:39 -0700

    recognition now works frontend -> backend -> api

--- frontend/app/src/main/java/com/cpen321/usermanagement/data/remote/api/WildlifeApi.kt
+++ frontend/app/src/main/java/com/cpen321/usermanagement/data/remote/api/WildlifeApi.kt
@@ -1,17 +1,13 @@
-package com.cpen321.usermanagement.data.remote.api
-
-import com.cpen321.usermanagement.data.remote.dto.WildlifeResponse
 import okhttp3.MultipartBody
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
-interface WildlifeApi {
-
+interface RecognitionApi {
     @Multipart
-    @POST("detect-label")
-    suspend fun recognizeAnimal(
-        @Part image: MultipartBody.Part,
-        @Header("x-rapidapi-key") apiKey: String = "b8b295a1e9msh04198519f49ce85p198b00jsn5742e43e1cfe",
-        @Header("x-rapidapi-host") host: String = "label-image.p.rapidapi.com"
-    ): Response<WildlifeResponse>
+    @POST("api/recognition")
+    suspend fun recognizeImage(
+        @Part image: MultipartBody.Part
+    ): Response<RecognitionResponse>
 }
