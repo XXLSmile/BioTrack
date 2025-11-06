497f97e900558a5f7c186abea2c6312878e9f052
Xiaohan Liu <leoliu050107@gmail.com>
2025-11-06 15:41:50 -0800

    undeclared variables

--- backend/src/recognition/recognition.controller.ts
+++ backend/src/recognition/recognition.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import type { Express } from 'express-serve-static-core';
 import path from 'path';
 import crypto from 'crypto';
 import mongoose from 'mongoose';
